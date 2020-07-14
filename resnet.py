be686226871ccfe3a668d429792e25dfbbcdc2b3
himazin331 <sion20022013@gmail.com>
2020-07-14 09:09:08 +0900

    ResNet50

--- resnet.py
+++ resnet.py
@@ -9,12 +9,11 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
 import tensorflow.keras.layers as kl
 
-# 残差ブロック
+# 残差ブロック(Bottleneckアーキテクチャ)
 class Res_Block(tf.keras.Model):
     def __init__(self, in_channels, out_channels):
         super().__init__()
 
-        # Bottleneckアーキテクチャ
         bneck_channels = out_channels // 4
 
         self.bn1 = kl.BatchNormalization()
@@ -111,27 +110,30 @@ class trainer(object):
 
     def train(self, train_img, train_lab, test_images, test_labels, out_path, batch_size, epochs):
         
+        print("\n\n___Start training...")
+
         his = self.resnet.fit(train_img, train_lab, batch_size=batch_size, epochs=epochs)
         
+        graph_output(his, out_path) # グラフ出力
+
         print("___Training finished\n\n")
                
-        test_loss, test_acc = self.resnet.evaluate(test_images, test_labels)
-        print("Test acc: {}".format(test_acc))
+        self.resnet.evaluate(test_images, test_labels) # テストデータ推論
 
-        print("___Saving parameter...")
+        print("\n___Saving parameter...")
         out_path = os.path.join(out_path, "resnet.h5")
-        self.resnet.save_weights(out_path)
+        self.resnet.save_weights(out_path) # パラメータ保存
         print("___Successfully completed\n\n")
 
-        return his
-
-def graph_output(history):
+# accuracy, lossグラフ
+def graph_output(history, out_path):
     
     plt.plot(history.history['accuracy'])
     plt.title('Model accuracy')
     plt.ylabel('Accuracy')
     plt.xlabel('Epoch')
     plt.legend(['Train'], loc='upper left')
+    plt.savefig(os.path.join(out_path, "acc_graph.jpg"))
     plt.show()  
 
     plt.plot(history.history['loss'])
@@ -139,13 +141,10 @@ def graph_output(history):
     plt.ylabel('Loss')
     plt.xlabel('Epoch')
     plt.legend(['Train'], loc='upper left')
+    plt.savefig(os.path.join(out_path, "loss_graph.jpg"))
     plt.show()
 
 def main():
-
-    # プログラム情報
-    print("ResNet50 ver.2")
-    print("Last update date:    2020/05/08\n")
     
     # コマンドラインオプション作成
     parser = arg.ArgumentParser(description='ResNet50')
@@ -164,7 +163,7 @@ def main():
     print("# Minibatch-size: {}".format(args.batch_size))
     print("# Epoch: {}".format(args.epoch))
     print("===========================")
-    
+
     # 出力フォルダの作成(フォルダが存在する場合は作成しない)
     os.makedirs(args.out, exist_ok=True)
 
@@ -173,15 +172,13 @@ def main():
     (train_images, train_labels), (test_images, test_labels) = f_mnist.load_data()
 
     # 画像データ加工
-    train_images /= 255.0
-    train_images = train_images[:, :, :, np.newaxis]
-    test_images /= 255.0
-    test_images = test_images[:, :, :, np.newaxis]
+    train_imgs = train_images / 255.0
+    train_imgs = train_imgs[:, :, :, np.newaxis]
+    test_imgs = test_images / 255.0
+    test_imgs = test_imgs[:, :, :, np.newaxis]
     
     Trainer = trainer()
-    his = Trainer.train(train_images, train_labels, test_images, test_labels, args.out, args.batch_size, args.epoch)
-
-    graph_output(his)
+    Trainer.train(train_imgs, train_labels, test_imgs, test_labels, args.out, args.batch_size, args.epoch)
 
 if __name__ == "__main__":
     main()
